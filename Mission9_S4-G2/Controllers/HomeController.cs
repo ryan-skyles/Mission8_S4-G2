9e55b4d13c540a5feb122308d8cd8def010c9e85
ryan-skyles <ryan.skyles01@gmail.com>
2026-02-24 12:30:05 -0700

    Add project files.

---
+++ Mission9_S4-G2/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission9_S4_G2.Models;
+using System.Diagnostics;
+
+namespace Mission9_S4_G2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission9_S4-G2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission9_S4_G2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
